d07bc338b09b7a9ae2e9b4bef25e40d7f9cfcadb
jacob gichira mbuco <mbucojacob@gmail.com>
2026-03-25 18:36:31 +0300

    Search through an array.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+//a function that retyrns a year the denver broncos won the super bowl
+function superbowlWin(record) {
+    //use the find method to search through the record array for an object with a result of "W"
+    const win = record.find(game => game.result === "W");
+    //if a win is found, return the year, otherwise return undefined
+    return win ? win.year : undefined;
+}   
+
